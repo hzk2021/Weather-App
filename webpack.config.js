5ec919ca46249f2a44ee4af0d5c20edf855c9a8e
hzk2021 <hezhenkai1999@gmail.com>
2023-05-03 16:28:20 +0800

    Init Webpack configurations

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 9000,
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: ["style-loader", "css-loader", 'sass-loader'],
+            }
+        ]
+    }
+}
